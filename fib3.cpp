74811a54c869d5ce20ee3169efaab5fa81caea79
nabiloo6060 <118072654+nabiloo6060@users.noreply.github.com>
2022-12-22 16:38:56 +0330

    Add files via upload

---
+++ fib3.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+  
+int fib(int n) 
+{ 
+   if (n <= 1) 
+      return n; 
+   return fib(n-1) + fib(n-2); 
+} 
+  
+int main () 
+{ 
+  int n = 9; 
+  printf("%d", fib(n)); 
+  
+  return 0; 
+} 
